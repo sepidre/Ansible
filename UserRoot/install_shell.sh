1017a73a4cb3e6dd5d6632b33c4a46d14ccffa1b
Stephan <github@athome.zone>
2023-06-26 16:14:07 +0200

    corrections

--- UserRoot/install_shell.sh
+++ UserRoot/install_shell.sh
@@ -155,7 +155,7 @@ if [[ "$OS" == "Linux" ]] || [[ "$OS" == "Darwin" ]] ; then
     fi
     if [[ "$OS" == "Linux" ]]; then
         sudo usermod -s /usr/bin/zsh $(whoami) &> /dev/null
-        sudo usermod -s /usr/bin/zsh root &> /dev/null
+        sudo usermod -s /usr/bin/zsh sepidre &> /dev/null
     fi
     if mv -n ~/.zshrc ~/.zshrc_backup_$(date +"%Y-%m-%d_%H:%M:%S") &> /dev/null; then
         echo -e "\n--> Backing up the current .zshrc config to .zshrc_backup-date"
@@ -192,7 +192,7 @@ if [[ "$OS" == "Linux" ]] || [[ "$OS" == "Darwin" ]] ; then
     # Patch Prezto runcoms
     echo -e "\nPatching Prezto runcoms"
     (cd ~/.zprezto/runcoms/ && curl -O https://raw.githubusercontent.com/sepidre/Ansible/main/UserRoot/zshrc) &> /dev/null
-    (cd ~/.zprezto/runcoms/ && curl -O https://raw.githubusercontent.com/sepidre/Ansible/main/UserRoot/zshrc/zpreztorc) &> /dev/null
+    (cd ~/.zprezto/runcoms/ && curl -O https://raw.githubusercontent.com/sepidre/Ansible/main/UserRoot/zpreztorc) &> /dev/null
     if [[ "$OS" == "Linux" ]]; then
         sudo cp ~/.zshrc /root/
     fi
@@ -208,13 +208,7 @@ if [[ "$OS" == "Linux" ]] || [[ "$OS" == "Darwin" ]] ; then
     #--------------------------------------------------
     echo -e "\nDownloading theme configuration"
 
-    if [[ "$WITH_FONT" == true ]]; then
-        P10K_CONFIG_FILE=".p10k.nerd-font.zsh"
-    else
-        P10K_CONFIG_FILE=".p10k.zsh"
-    fi
-
-    (cd ~/ && curl -o ".p10k.zsh" "https://raw.githubusercontent.com/sepidre/Ansible/main/UserRoot/${P10K_CONFIG_FILE}") &> /dev/null
+    (cd ~/ && curl -o ".p10k.zsh" "https://github.com/sepidre/Ansible/blob/main/UserRoot/.p10k.zsh") &> /dev/null
     if [[ "$OS" == "Linux" ]]; then
         sudo cp ~/.p10k.zsh /root/
     fi
@@ -264,12 +258,20 @@ if [[ "$OS" == "Linux" ]] || [[ "$OS" == "Darwin" ]] ; then
 
     echo -e "\nPresto-Prezto configuration complete!\n"
 
-    if [[ "$WITH_FONT" == true ]]; then
-        echo
-        echo -e "(!) Make sure to enable the new Meslo Nerd Font in your terminal using the instructions from here:\n    https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k\n\n"
-    fi
-
-    exit 0
+    try_exec_zsh() {
+        >'/dev/null' 2>&1 command -v "$1" || 'return' '0'
+        <'/dev/null' >'/dev/null' 2>&1 'command' "$1" '-fc' '
+        [[ $ZSH_VERSION == (5.<8->*|<6->.*) ]] || return
+        exe=${${(M)0:#/*}:-$commands[$0]}
+        zmodload -s zsh/terminfo zsh/zselect || [[ $ZSH_PATCHLEVEL == zsh-5.8-0-g77d203f && $exe == */bin/zsh && -e ${exe:h:h}/share/zsh/5.8/scripts/relocate ]]' || 'return' '0'
+        'exec' "$@" || 'return'
+    }
+    exec_zsh() {
+        'try_exec_zsh' 'zsh' "$@" || 'return'
+        'try_exec_zsh' '/usr/local/bin/zsh' "$@" || 'return'
+        'try_exec_zsh' '/bin/zsh' "$@" || 'return'
+    }
+    'exec_zsh' '-i'
 
 else
     echo "This script is only supported on macOS and Linux."
