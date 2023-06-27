73ae8ed9a7d47961ec480acaa7e4ad14bb2d97ec
Stephan <github@athome.zone>
2023-06-27 08:08:34 +0200

    removed some calls

--- UserRoot/install_shell.sh
+++ UserRoot/install_shell.sh
@@ -258,20 +258,6 @@ if [[ "$OS" == "Linux" ]] || [[ "$OS" == "Darwin" ]] ; then
 
     echo -e "\nPresto-Prezto configuration complete!\n"
 
-    try_exec_zsh() {
-        >'/dev/null' 2>&1 command -v "$1" || 'return' '0'
-        <'/dev/null' >'/dev/null' 2>&1 'command' "$1" '-fc' '
-        [[ $ZSH_VERSION == (5.<8->*|<6->.*) ]] || return
-        exe=${${(M)0:#/*}:-$commands[$0]}
-        zmodload -s zsh/terminfo zsh/zselect || [[ $ZSH_PATCHLEVEL == zsh-5.8-0-g77d203f && $exe == */bin/zsh && -e ${exe:h:h}/share/zsh/5.8/scripts/relocate ]]' || 'return' '0'
-        'exec' "$@" || 'return'
-    }
-    exec_zsh() {
-        'try_exec_zsh' 'zsh' "$@" || 'return'
-        'try_exec_zsh' '/usr/local/bin/zsh' "$@" || 'return'
-        'try_exec_zsh' '/bin/zsh' "$@" || 'return'
-    }
-    'exec_zsh' '-i'
     exit 0
 else
     echo "This script is only supported on macOS and Linux."
